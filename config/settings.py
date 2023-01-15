69bab747896df0049f65d357b925621c5b7f1aa4
vijay <vijaykumarpatil2000@gmail.com>
2023-01-15 13:05:07 +0530

    heroku devlopment

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-dywqftj&_jr9mz%mrke$(l69xo59tj#-a$-5-lb)kt93(lj*_2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,11 @@
+'''
 from django.urls import path
 from .views import homePageView
 urlpatterns = [
 path('', homePageView, name='home'),
+]'''
+from django.urls import path
+from .views import HomePageView
+urlpatterns = [
+path('', HomePageView.as_view(), name='home'),
 ]
